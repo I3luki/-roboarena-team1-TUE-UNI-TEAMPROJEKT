03f9c8966969d2cf24cd29a4c49a108fdd987928
I3luki <dennis.andler@gmail.com>
2026-04-29 01:29:32 +0200

    add spawning apple

--- src/snake/main.py
+++ src/snake/main.py
@@ -1,9 +1,11 @@
 import pygame
+from random import randrange
 
 
 WINDOW_WIDTH = 600
 WINDOW_HEIGHT = 600
 SNAKE_BLOCK_SIZE = 30
+APPLE_BLOCK_SIZE = 15
 FPS = 10
 
 
@@ -19,9 +21,14 @@ def main():
     running = True
     direction = "RIGHT"
 
-    # Startposition (Mitte des Bildschirms)
+    # Startposition Schlange (Mitte des Bildschirms)
     # Die Schlange ist eine Liste aus Segmenten [x, y]
     snake = [[300, 300], [270, 300], [240, 300]]
+
+    # Startposition Apfel (2.Zeile, 2.Reihe)
+    # Der Apfel besteht aus einem Segment [x,y]
+    apple = [SNAKE_BLOCK_SIZE,SNAKE_BLOCK_SIZE]
+
 
     while running:
         # Events verarbeiten
@@ -52,6 +59,23 @@ def main():
         elif direction == "RIGHT":
             head_x += SNAKE_BLOCK_SIZE
 
+        # Testen ob Schlangenkopf Apfel berührt
+        #   TRUE  --    setze Apfel auf zufaellige Position
+        #   FALSE --    tue nichts
+        def IsSnakeheadTouchingApple():
+            snakehead_x = snake[0][0]
+            snakehead_y = snake[0][1]
+            apple_x = apple[0]
+            apple_y = apple[1]
+            if(apple_x == snakehead_x and apple_y == snakehead_y):
+                return True
+            return False
+        
+        if(IsSnakeheadTouchingApple()): 
+            apple[0] = randrange(int(WINDOW_WIDTH/SNAKE_BLOCK_SIZE)) * SNAKE_BLOCK_SIZE
+            apple[1] = randrange(int(WINDOW_HEIGHT/SNAKE_BLOCK_SIZE)) * SNAKE_BLOCK_SIZE
+            
+
         # Neuen Kopf hinzufügen
         new_head = [head_x, head_y]
         snake.insert(0, new_head)
@@ -65,6 +89,13 @@ def main():
         # Schlange zeichnen
         for segment in snake:
             pygame.draw.rect(screen, "green", [segment[0], segment[1], SNAKE_BLOCK_SIZE, SNAKE_BLOCK_SIZE])
+
+        # Apfel zeichnen
+        # offset fuer apfelgroesse kleiner als schlangengröße
+        apple_draw_offset = int((SNAKE_BLOCK_SIZE - APPLE_BLOCK_SIZE) / 2)
+        pygame.draw.rect(screen, "red", [ apple[0] + apple_draw_offset,
+                                          apple[1] + apple_draw_offset, 
+                                          APPLE_BLOCK_SIZE, APPLE_BLOCK_SIZE ])
 
         pygame.display.flip()
 
