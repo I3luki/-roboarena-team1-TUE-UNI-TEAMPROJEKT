5c5e72abb71fcbb0ae6a2769ebb4551d5e7981d4
zxohi43 <noah.utech@student.uni-tuebingen.de>
2026-05-06 12:05:28 +0200

    draw robot

--- src/Sprint2_RoboArena/main.py
+++ src/Sprint2_RoboArena/main.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 from Arena import Arena  # ← importieren
+from Roboter import Robot
 
 pygame.init()
 screen = pygame.display.set_mode((1000, 1000))
@@ -9,6 +10,7 @@ clock = pygame.time.Clock()
 
 # Create arena object
 arena = Arena(screen)
+robot = Robot(screen, 475, 475)
 
 while True:
     for event in pygame.event.get():
@@ -16,8 +18,12 @@ while True:
             pygame.quit()
             exit()
 
+    keys = pygame.key.get_pressed()
+    robot.move(keys)
+
     # Draw arena
     arena.draw()
+    robot.draw()
 
     pygame.display.update()
     clock.tick(60)
