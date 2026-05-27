19d5e21c0f06c5c61890c829ae804bb3d8b179ff
I3luki <dennis.andler@gmail.com>
2026-05-27 14:41:14 +0200

    refactor main gameloop into methods

--- src/Sprint2_RoboArena/main.py
+++ src/Sprint2_RoboArena/main.py
@@ -13,6 +13,65 @@ SCREEN_WIDTH = 1000
 SCREEN_HEIGHT = 1000
 
 
+
+
+# Update alles
+def update():
+    #Input-Berreich
+    keys = pygame.key.get_pressed()
+
+    #Updates-Berreich
+    robot.move(keys)
+    robot.update_rotation()
+    arena.update_lightning_tiles(robot, health)
+    arena.update_tornado(robot, health)
+    enemy_manager.update()
+    for enemy in enemy_manager.enemies:
+        enemy.draw()
+        enemy.check_damage_player(robot, health)
+    robot.update_attack(enemy_manager.enemies) # Updated Attacke/Damage von Roboter an Gegner
+    robot.update_status_effects()
+
+
+    # Checke für Kollision von Roboter und Orb 
+    # TODO: REFACTOR IT
+    for orb in orb_list[:]:
+        if robot.aabb.check_collision(orb.aabb):
+            level.collect_orb()
+            orb.randomize_position()
+
+
+# Zeichne alles
+def draw():
+    arena.draw(robot)
+    robot.draw()
+    for orb in orb_list:
+        orb.draw()
+    for enemy in enemy_manager.enemies:
+        enemy.draw()
+
+    health.draw()
+    stamina.draw()
+    level.draw()
+
+
+# Testmodus
+# "visualisiert ausgewählte hintergrundberechnungen und andere testbedingte werte"
+def test_mode():
+    if(TEST_MODE):
+        for orb in orb_list:
+            orb.draw_aabb() 
+        robot.draw_aabb()
+        robot.draw_line_to_mouse()
+        for enemy in enemy_manager.enemies:
+            enemy.draw_aabb()
+            enemy.draw_line_enemy(robot)
+        arena.draw_aabb()
+
+
+
+
+# -------------------------------------------------------------------- INITIATION ------------
 pygame.init()
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("RoboArena")
@@ -44,61 +103,17 @@ for enemy in enemy_manager.enemies:
 
 
 
+# -------------------------------------------------------------------- GAME LOOP ------------
 while True:
 
-    #Envents-Berreich
+    # Check for Quit
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             exit()
 
-    #Input-Berreich
-    keys = pygame.key.get_pressed()
+    update()  # update all objects
+    draw()    # draw all objects
 
-    #Updates-Berreich
-    robot.move(keys)
-    robot.update_rotation()
-    arena.update_lightning_tiles(robot, health)
-    arena.update_tornado(robot, health)
-    enemy_manager.update()
-    for enemy in enemy_manager.enemies:
-        enemy.draw()
-        enemy.check_damage_player(robot, health)
-    robot.update_attack(enemy_manager.enemies) # Updated Attacke/Damage von Roboter an Gegner
-
-    robot.update_status_effects()
-
-
-    # Checke für Kollision von Roboter und Orb
-    for orb in orb_list[:]:
-        if robot.aabb.check_collision(orb.aabb):
-            level.collect_orb()
-            orb.randomize_position()
-
-    #Draw-Berreich
-    # Zeichne Objekte auf den Screen
-    arena.draw(robot)
-    robot.draw()
-    for orb in orb_list:
-        orb.draw()
-    for enemy in enemy_manager.enemies:
-        enemy.draw()
-
-    health.draw()
-    stamina.draw()
-    level.draw()
-
-
-    # Testmodus
-    # "visualisiert ausgewählte hintergrundberechnungen und andere testbedingte werte"
-    if(TEST_MODE):
-        for orb in orb_list:
-            orb.draw_aabb() 
-        robot.draw_aabb()
-        robot.draw_line_to_mouse()
-        for enemy in enemy_manager.enemies:
-            enemy.draw_aabb()
-            enemy.draw_line_enemy(robot)
-        arena.draw_aabb()
     pygame.display.flip() #update screen
-    clock.tick(60)
+    clock.tick(60) # wait until next frametime
